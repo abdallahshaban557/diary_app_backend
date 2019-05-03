0ffebd22d50e6a0c94096d3566a08a4ad9df4921
abdullahshaaban <abdullah.shaaban91@gmail.com>
2019-05-02 17:53:15 -0700

    minor cleanup

--- app.py
+++ app.py
@@ -1,67 +1,21 @@
 import time
 import json
-from flask import Flask,request, Response,jsonify
-from functools import wraps
-#Push notification library
-from apns2.client import APNsClient
-from apns2.payload import Payload
-#DynamoDB client
-import boto3
-from boto3.dynamodb.conditions import Key, Attr
-#Needed to create primary hashed key for the dynamodb items
-import uuid
-from flask_apscheduler import APScheduler
+from flask import Flask, request, Response, jsonify
 
 app = Flask(__name__)
 
-#Configuration for the queue for resending notification
-# class Config(object):
-#     JOBS = [
-#         {
-#             'id': 'job1',
-#             'func': 'app:resend_notification',
-#             'trigger': 'interval',
-#             'seconds': 300
-#         }
-#     ]
-#     SCHEDULER_API_ENABLED = True
-
-#The function responsible for scanning through the BOPUS orders, and sending a reminder notification
-def resend_notification():    
-    Overdue_Notifications = notification_records.scan( FilterExpression=Attr('ReadReceiptStatus').eq(0) )
-    Pending_Store_Orders = []
-    for notification in Overdue_Notifications['Items']:
-        if(notification["StoreID"] in Pending_Store_Orders):
-            continue
-        else:
-            Pending_Store_Orders.append(notification["StoreID"])
-    for Pending_Stores in Pending_Store_Orders:
-        All_Devices = store_information.scan( FilterExpression=Attr('StoreID').eq(Pending_Stores))
-        for Device in All_Devices['Items']:
-            try:    
-                #print('Sending Notification')
-                print("Sending")
-                sendpushnotification(Device["DeviceToken"], "Reminder" , 0, 0)
-            except:
-                print("ERROR Sending")
-                store_information.delete_item(
-                    Key={
-                    "ID" : Device["ID"] 
-                    }
-                )
-                pass
-            
-            
-#Checks username and password
+# Checks username and password
 def check_auth(username, password):
-    return username == 'petco' and password == 'petco123'
-#Returns if authenticated or not
+    return username == 'diary' and password == 'diaryapp'
+# Returns if authenticated or not
+
 def authenticate():
     return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
-#creates the decorator the enables auth on endpoints
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+# creates the decorator the enables auth on endpoints
+
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -71,102 +25,85 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
-#DynamoDB connection
-client = boto3.resource('dynamodb', region_name='us-east-1')
-
-
-#Connection to the specific Tables  
-notification_records = client.Table('store_partner_notification')
-store_information = client.Table('store_information')
-
-def sendpushnotification(DeviceToken, OrderID, StoreID, dev_flag):
-    #send the push notification
-    custom = {'launchURL': 'x-com.petco.wrapper.sim://launch' }
-    payload = Payload(alert= "New BOPUS order is ready", sound="popcorn.wav", badge=1, custom=custom)
-    topic = 'com.petco.notifications'
-    IOS_Client = APNsClient('./Apple_Certificate/server1.pem', use_sandbox= dev_flag, use_alternative_port=False)
-    IOS_Client.send_notification(DeviceToken, payload, topic)
-    return True
-
 @app.route('/')
 def hello():
-    return jsonify({"Success" :True})
-    
-@app.route('/deleteallreadnotifications', methods = ['DELETE'])
+    return jsonify({"Success": True})
+
+@app.route('/deleteallreadnotifications', methods=['DELETE'])
 @requires_auth
 def deleteallreadnotifications():
-    Notifications_Search = notification_records.scan( FilterExpression=Attr('ReadReceiptStatus').eq(1))
+    Notifications_Search = notification_records.scan(
+        FilterExpression=Attr('ReadReceiptStatus').eq(1))
     for notification in Notifications_Search["Items"]:
-        notification_records.delete_item(Key = {
-            "ID" : notification["ID"]
+        notification_records.delete_item(Key={
+            "ID": notification["ID"]
         })
-    return jsonify({"Success" : True})
+    return jsonify({"Success": True})
 
-
-@app.route('/deleteallnotifications', methods = ['DELETE'])
+@app.route('/deleteallnotifications', methods=['DELETE'])
 @requires_auth
 def deleteallnotifications():
     Notifications_Search = notification_records.scan()
     for notification in Notifications_Search["Items"]:
-        notification_records.delete_item(Key = {
-            "ID" : notification["ID"]
+        notification_records.delete_item(Key={
+            "ID": notification["ID"]
         })
-    return jsonify({"Success" : True})
+    return jsonify({"Success": True})
 
-#endpoint to get all of the notifications in DynamoDB
+# endpoint to get all of the notifications in DynamoDB
 @app.route('/getallnotificationrecords')
 @requires_auth
 def getallnotificationrecords():
     notifications = []
-    Notifications_Search = notification_records.scan() 
+    Notifications_Search = notification_records.scan()
     for notification in Notifications_Search["Items"]:
         notifications.append({
-            "OrderID" : notification["OrderID"],
-            "OrderCreationDate" : notification["OrderCreationDate"],
-            "StoreID" : int(notification["StoreID"]),
-            "NotificationCreationDate" : notification["NotificationCreationDate"],
-            "ReadReceiptStatus" : int(notification["ReadReceiptStatus"])
-            }
+            "OrderID": notification["OrderID"],
+            "OrderCreationDate": notification["OrderCreationDate"],
+            "StoreID": int(notification["StoreID"]),
+            "NotificationCreationDate": notification["NotificationCreationDate"],
+            "ReadReceiptStatus": int(notification["ReadReceiptStatus"])
+        }
         )
-    return jsonify({"Success" : True , "Payload" : notifications})
+    return jsonify({"Success": True, "Payload": notifications})
 
-#New order submitted from OMS
+# New order submitted from OMS
 @app.route('/addorder', methods=['POST'])
 @requires_auth
-def addorder():    
-    #change request received through endpoint to JSON
+def addorder():
+    # change request received through endpoint to JSON
     Payload = request.json
-    #create the insert object into DB
+    # create the insert object into DB
     BOPUS_Order = {
-                "ID" : uuid.uuid4().hex,
-                "OrderID" : Payload["OrderID"],
-                "OrderCreationDate" : Payload["OrderCreationDate"],
-                "StoreID" : int(Payload["StoreID"]),
-                "NotificationCreationDate" : time.strftime('%x %X'),
-                "ReadReceiptStatus" : 0,
+        "ID": uuid.uuid4().hex,
+        "OrderID": Payload["OrderID"],
+        "OrderCreationDate": Payload["OrderCreationDate"],
+        "StoreID": int(Payload["StoreID"]),
+        "NotificationCreationDate": time.strftime('%x %X'),
+        "ReadReceiptStatus": 0,
     }
-    #inset object into Dynamodb - commented out based on requirement from store Ops
-    # if Payload["dev_flag"] == False:
-    #     notification_records.put_item(Item = BOPUS_Order)
 
-    response = store_information.scan( FilterExpression=Attr('StoreID').eq(Payload["StoreID"]) )
-    #Find all devices attached to the specified store, and send notification - Try/except to skip if a notification error occurs
+    response = store_information.scan(
+        FilterExpression=Attr('StoreID').eq(Payload["StoreID"]))
+    # Find all devices attached to the specified store, and send notification - Try/except to skip if a notification error occurs
     if Payload["dev_flag"] == False:
         for Device in response['Items']:
             try:
-                sendpushnotification(Device["DeviceToken"], Payload["OrderID"],Payload["StoreID"], False)
+                sendpushnotification(
+                    Device["DeviceToken"], Payload["OrderID"], Payload["StoreID"], False)
             except:
                 pass
-    return jsonify({"Success" : True})    
+    return jsonify({"Success": True})
 
-#Indicate that the store received the notification
+# Indicate that the store received the notification
 @app.route('/readnotification', methods=['POST'])
 @requires_auth
 def readnotification():
     Payload = request.json
     StoreID = int(Payload["StoreID"])
-    Notification_Search = notification_records.scan( FilterExpression=Attr('StoreID').eq(StoreID))    
-    for notification in Notification_Search["Items"]:      
+    Notification_Search = notification_records.scan(
+        FilterExpression=Attr('StoreID').eq(StoreID))
+    for notification in Notification_Search["Items"]:
         # notification_records.update_item(
         #     Key= {
         #         "ID" : notification["ID"]
@@ -176,106 +113,106 @@ def readnotification():
         #     ':val1': 1
         # })
         notification_records.delete_item(
-            Key= {
-                "ID" : notification["ID"]
+            Key={
+                "ID": notification["ID"]
             }
         )
-    return jsonify({"Success" : True})
+    return jsonify({"Success": True})
 
-#register device token
+# register device token
 @app.route('/registerdevice', methods=['POST'])
 @requires_auth
 def registerdevicetoken():
-    #change request to JSON and grab the required variables
+    # change request to JSON and grab the required variables
     Payload = request.json
     DeviceToken = Payload["DeviceToken"]
     StoreID = Payload["StoreID"]
-    #check if the store exists in MongoDB
-    Device_Search = store_information.scan( FilterExpression=Attr('DeviceToken').eq(DeviceToken))    
-  
+    # check if the store exists in MongoDB
+    Device_Search = store_information.scan(
+        FilterExpression=Attr('DeviceToken').eq(DeviceToken))
+
     if (Device_Search["Count"] == 0):
-        store_information.put_item(Item = {"ID" : uuid.uuid4().hex, "DeviceToken" : DeviceToken, "StoreID" : StoreID})
+        store_information.put_item(
+            Item={"ID": uuid.uuid4().hex, "DeviceToken": DeviceToken, "StoreID": StoreID})
     else:
         for Device in Device_Search["Items"]:
             store_information.update_item(
-        Key={
-            'ID': Device["ID"]
-        },
-        UpdateExpression='SET StoreID = :val1',
-        ExpressionAttributeValues={
-            ':val1': StoreID
-        }
-        )   
-    return jsonify({"Success" : True})
+                Key={
+                    'ID': Device["ID"]
+                },
+                UpdateExpression='SET StoreID = :val1',
+                ExpressionAttributeValues={
+                    ':val1': StoreID
+                }
+            )
+    return jsonify({"Success": True})
+
 
 @app.route('/getallregistereddevices', methods=['GET'])
 @requires_auth
 def getallregistereddevices():
-    #change request to JSON and grab the required variables
+    # change request to JSON and grab the required variables
     Registerd_Devices = []
-    #find all devices
+    # find all devices
     Devices = store_information.scan()
     for device in Devices["Items"]:
-        Registerd_Devices.append( {
-            "StoreID" : int(device["StoreID"]),
-            "DeviceToken" : device["DeviceToken"]
-            }
+        Registerd_Devices.append({
+            "StoreID": int(device["StoreID"]),
+            "DeviceToken": device["DeviceToken"]
+        }
         )
-    return jsonify({"Success" : True , "Payload" : Registerd_Devices})
+    return jsonify({"Success": True, "Payload": Registerd_Devices})
+
 
 @app.route('/deletealldevices', methods=['DELETE'])
 @requires_auth
 def deletealldevices():
-    #change request to JSON and grab the required variables
+    # change request to JSON and grab the required variables
     response = store_information.scan()
     for device in response['Items']:
-        store_information.delete_item(Key={"ID" : device["ID"]})     
-    return jsonify({"Success" : True})
+        store_information.delete_item(Key={"ID": device["ID"]})
+    return jsonify({"Success": True})
+
 
 @app.route('/sendpushnotification', methods=['POST'])
 @requires_auth
 def pushnotification():
     Payload = request.json
-    sendpushnotification(Payload["DeviceToken"], Payload["OrderID"],Payload["StoreID"], Payload["dev_flag"])
+    sendpushnotification(
+        Payload["DeviceToken"], Payload["OrderID"], Payload["StoreID"], Payload["dev_flag"])
     return jsonify({"Sucess": True})
 
-#Finds all of the registered devices for a store
+# Finds all of the registered devices for a store
 @app.route('/CheckRegisteredDevices/<int:StoreID>', methods=['GET'])
 @requires_auth
 def CheckRegisteredDevices(StoreID):
     Registerd_Devices = []
-    Devices = store_information.scan(FilterExpression=Attr('StoreID').eq(StoreID) )
+    Devices = store_information.scan(
+        FilterExpression=Attr('StoreID').eq(StoreID))
     for device in Devices["Items"]:
-        Registerd_Devices.append( {
-            "StoreID" : int(device["StoreID"]),
-            "DeviceToken" : device["DeviceToken"]
-            }
+        Registerd_Devices.append({
+            "StoreID": int(device["StoreID"]),
+            "DeviceToken": device["DeviceToken"]
+        }
         )
-    return jsonify({"Success" : True , "Payload" : Registerd_Devices})
+    return jsonify({"Success": True, "Payload": Registerd_Devices})
 
-#Find alerts that have not been acknowledged in a store
+# Find alerts that have not been acknowledged in a store
 @app.route('/CheckUnreadAlerts/<int:StoreID>', methods=['GET'])
 @requires_auth
 def CheckUnreadAlerts(StoreID):
     Unread_Alerts = []
-    Alerts = notification_records.scan(FilterExpression=Attr('StoreID').eq(StoreID) & Attr('ReadReceiptStatus').eq(0))
+    Alerts = notification_records.scan(FilterExpression=Attr(
+        'StoreID').eq(StoreID) & Attr('ReadReceiptStatus').eq(0))
     for Alert in Alerts["Items"]:
-        Unread_Alerts.append( {
-            "OrderID" : Alert["OrderID"],
-            "ReadReceiptStatus" : int(Alert["ReadReceiptStatus"])
-            }
+        Unread_Alerts.append({
+            "OrderID": Alert["OrderID"],
+            "ReadReceiptStatus": int(Alert["ReadReceiptStatus"])
+        }
         )
-    return jsonify({"Success" : True , "Payload" :  Unread_Alerts})
+    return jsonify({"Success": True, "Payload":  Unread_Alerts})
 
 
 if __name__ == "__main__":
-    #Configure the queue for resending notifications
-    # app.config.from_object(Config())
-    # scheduler = APScheduler()
-    # scheduler.init_app(app)
-    # scheduler.start()
-
-
-
-    #Running the flask app
-    app.run(host="0.0.0.0", ssl_context='adhoc') 
+    # Running the flask app
+    app.run(host="0.0.0.0", ssl_context='adhoc')
