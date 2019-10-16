13a3e0b6b6c2864576cafa83eb9e7393977d399a
Abdallah Shaban <abdullah.shaaban91@gmail.com>
2019-10-15 17:45:45 -0700

    updated usermodel to accept longer userID

--- models/DiaryEntry.py
+++ models/DiaryEntry.py
@@ -6,4 +6,4 @@ class DiaryEntries(Document):
     body = StringField(max_length=10000)
     entryDate = StringField(max_length=50)
     updateTS = StringField(max_length=50)
-    userID = StringField(max_length=20)
+    userID = StringField(max_length=50)
